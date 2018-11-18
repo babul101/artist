1b65c99753b67930ca4765a0cbc37cd1b61d867c
babul101 <babuldeb101@gmail.com>
2018-11-18 17:10:10 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter,Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Components
+import Home from './components/home';
+import Artist from './components/artist';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Route exact path = '/' component={Home}></Route>
+                <Route path = '/artist/:artistid' component={Artist}></Route>
+            </div>
+                
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+
+//To start the json web server
+// json-server --watch db.json --port 3004
